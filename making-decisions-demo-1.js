bb4cfac8070e5ce6311d3780ce56396b3d44130e
Caleb Hodges <chodgesdev@gmail.com>
2022-11-15 20:32:58 -0600

    Initial Commit

---
+++ making-decisions-demo-1.js
@@ -0,0 +1,57 @@
+let jonSnowAttack = 25 
+let jamieLannisterAttack = 35
+
+let jonSnowHealth = 100
+let jonSnowDefense = 10
+
+// MY ANSWER
+// if (jonSnowHealth > jamieLannisterAttack - jonSnowDefense) {
+//     console.log("Jon Snow lives")
+// } else {
+//     console.log("Jon Snow is dead!")
+// }
+
+// INSTRUCTOR ANSWER
+// if(jonSnowHealth <= jamieLannisterAttack - jonSnowDefense) {
+//     console.log("See ya Jon")
+// } else {
+//     jonSnowHealth -= (jamieLannisterAttack - jonSnowDefense)
+//     console.log(`Jon has ${jonSnowHealth} health`)
+// }
+
+// MY ANSWER 
+// let healthKit = 50 
+
+// if (jonSnowHealth < 100) {
+//     jonSnowHealth += healthKit
+//     console.log(`Jon has ${jonSnowHealth} health after health kit`)
+// }
+
+// INSTRUCTOR ANSWER
+// if (jonSnowHealth + 50 >= 100) {
+//     jonSnowHealth = 100
+//     console.log(`Jon is back to full health!`)
+// } else {
+//     jonSnowHealth += 50
+//     console.log(`Jon's health has been increased to ${jonSnowHealth}`)
+// }
+
+// MY ANSWER
+// let isHeads = true
+
+// if (isHeads) {
+//     console.log("Jamie will keep fighting")
+// } else {
+//     console.log("Jon is allowed to escape")
+// }
+
+// INSTRUCTOR ANSWER
+ let coinLandsHeads = true
+
+ if(coinLandsHeads === true) {
+    console.log("The fight continues")
+ } else {
+    console.log("Jon is allowed to run away")
+ }
+
+
